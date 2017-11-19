6eb55992dcf57717015859e20e0d3c253674b030
dirathea <dira.thea@gmail.com>
2017-11-19 14:27:40 +0700

    basic reply message

--- line/handler.js
+++ line/handler.js
@@ -4,8 +4,27 @@ class Handler {
         this.ytsClient = ytsClient;
     }
 
-    handleRequest(events) {
-        console.log(events);
+    handleRequest(payload) {
+        console.log(payload);
+        payload.events.forEach(event => {
+            this.handleEvent(event);
+        });
+    }
+
+    handleEvent(event) {
+        const type = event.type;
+        switch (type) {
+            case 'message':
+                this.lineClient.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: `Hello.. I'm YTS Bot, will help you search for movies from YIFY collections...`
+                });
+                break;
+        
+            default:
+                console.log(`Unknown type ${type}`);
+                break;
+        }
     }
 }
 
