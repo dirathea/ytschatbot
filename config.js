e651f4a67d53feaff9470c987604bb9520ff074d
dirathea <dira.thea@gmail.com>
2017-11-21 23:27:49 +0700

    JSON Parse private key

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     YTS_BASE_URL: process.env.YTS_BASE_URL || 'https://yts.ag/api/v2/',
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_PRIVATE_KEY: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
     FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL
 }
