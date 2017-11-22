a6f3a9725a0c1deaa8c1edf9a122446f53c40f7f
dirathea <dira.thea@gmail.com>
2017-11-22 08:20:00 +0700

    Add all tracker

--- homepage/src/pages/WatchPage.js
+++ homepage/src/pages/WatchPage.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import * as firebase from "firebase";
-const trackers = ['wss://tracker.btorrent.xyz', 'wss://tracker.openwebtorrent.com', 'wss://tracker.fastcast.nz']
+const trackers = [
+  'wss://tracker.btorrent.xyz',
+  'wss://tracker.openwebtorrent.com',
+  'wss://tracker.fastcast.nz',
+  'udp://glotorrents.pw:6969/announce',
+  'udp://tracker.opentrackr.org:1337/announce',
+  'udp://torrent.gresille.org:80/announce',
+  'udp://tracker.openbittorrent.com:80',
+  'udp://tracker.coppersurfer.tk:6969',
+  'udp://tracker.leechers-paradise.org:6969',
+  'udp://p4p.arenabg.ch:1337',
+  'udp://tracker.internetwarriors.net:1337',
+];
 
 const rtcConfig = {
   'iceServers': [
