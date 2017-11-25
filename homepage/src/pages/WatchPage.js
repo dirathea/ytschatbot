e2a658cbe473f0a0d764130240b799694406d499
dirathea <dira.thea@gmail.com>
2017-11-25 15:52:41 +0700

    directly use id

--- homepage/src/pages/WatchPage.js
+++ homepage/src/pages/WatchPage.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import * as firebase from "firebase";
 
 class WatchPage extends Component {
-  state = {
-    video : ''
-  }
 
   constructor(props) {
     super(props);
-    this.db = firebase.firestore();
-  }
-
-  componentDidMount() {
-    this.db.doc(`session/${this.props.match.params.id}`).get().then(snapshot => {
-      this.setState({video: snapshot.id()});
-    });
   }
 
   render() {
     return (
       <div>
-        <video src={`/data/${this.state.video}`} />
+        <video src={`/data/${this.props.match.params.id}`} />
       </div>
     );
   }
