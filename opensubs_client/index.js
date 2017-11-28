16c68f6d00c114353873592bfc1841ff722ed4eb
dirathea <dira.thea@gmail.com>
2017-11-28 23:27:10 +0700

    dont download twice

--- opensubs_client/index.js
+++ opensubs_client/index.js
@@ -15,66 +15,74 @@ class OpenSubsClient {
     }
     getSubsLink(id, params) {
         console.log("Searching subs");
-        const searchParams = Object.assign(params);
-        console.log(searchParams);
-        return osClient.search(searchParams)
-        .then(subtitles => {
-            const downloadProcess = Object.keys(subtitles)
-                .map(lang => {
-                    return this.downloadSubtitle(subtitles[lang].url)
-                        .then(stream => {
-                            return new Promise((resolve, reject) => {
-                                const subtitleFile = this.bucket.file(`subtitles/${id}/${lang}/${subtitles[lang].filename.replace('.srt', '.vtt')}`);
-                                stream
-                                .pipe(srtToVtt())
-                                .pipe(subtitleFile.createWriteStream())
-                                    .on('error', err => console.log(err))
-                                    .on('finish', () => {
-                                        console.log(subtitles[lang]);
-                                        const expiredDate = new Date();
-                                        expiredDate.setMonth(expiredDate.getMonth() + 1);
-                                        subtitleFile.getSignedUrl({
-                                            action: 'read',
-                                            expires: expiredDate.toString(),
-                                        }, (err, url) => {
-                                            if (err) {
-                                                console.log(err);
-                                                return resolve({
-                                                    lang: subtitles[lang].code,
-                                                    langName: subtitles[lang].lang,
-                                                    url: ''
+        return this.firestore.doc(`/subs/${params.imdbid}`)
+            .get()
+            .then(snapshot => {
+                if (snapshot.exists) {
+                    return this.firestore.doc(`/session/${id}`)
+                        .update({subs: snapshot.doc()});
+                }
+                return osClient.search(params)
+                .then(subtitles => {
+                    const downloadProcess = Object.keys(subtitles)
+                        .map(lang => {
+                            return this.downloadSubtitle(subtitles[lang].url)
+                                .then(stream => {
+                                    return new Promise((resolve, reject) => {
+                                        const subtitleFile = this.bucket.file(`subtitles/${params.imdbid}/${lang}/${subtitles[lang].filename.replace('.srt', '.vtt')}`);
+                                        stream
+                                        .pipe(srtToVtt())
+                                        .pipe(subtitleFile.createWriteStream())
+                                            .on('error', err => console.log(err))
+                                            .on('finish', () => {
+                                                console.log(subtitles[lang]);
+                                                const expiredDate = new Date();
+                                                expiredDate.setMonth(expiredDate.getMonth() + 1);
+                                                subtitleFile.getSignedUrl({
+                                                    action: 'read',
+                                                    expires: expiredDate.toString(),
+                                                }, (err, url) => {
+                                                    if (err) {
+                                                        console.log(err);
+                                                        return resolve({
+                                                            lang: subtitles[lang].code,
+                                                            langName: subtitles[lang].lang,
+                                                            url: ''
+                                                        });
+                                                    } else {
+                                                        return resolve({
+                                                            lang: subtitles[lang].code,
+                                                            langName: subtitles[lang].lang,
+                                                            url
+                                                        });
+                                                    }
                                                 });
-                                            } else {
-                                                return resolve({
-                                                    lang: subtitles[lang].code,
-                                                    langName: subtitles[lang].lang,
-                                                    url
-                                                });
-                                            }
-                                        });
-                                    })
+                                            })
+                                    });
+                                });
+                        });
+                        return Promise.all(downloadProcess)
+                            .then(result => {
+                                console.log(result);
+                                this.firestore.doc(`/subs/${params.imdbid}`)
+                                    .update({subs: result});
+                                this.firestore.doc(`/session/${id}`)
+                                .update({subs: result})
+                                .then(() => {
+                                    console.log(`All supported subs ready`);
+                                    return Promise.resolve();
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return Promise.resolve();
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                return Promise.resolve();
                             });
-                        });
                 });
-                return Promise.all(downloadProcess)
-                    .then(result => {
-                        console.log(result);
-                        this.firestore.doc(`/session/${id}`)
-                        .update({subs: result})
-                        .then(() => {
-                            console.log(`All supported subs ready`);
-                            return Promise.resolve();
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            return Promise.resolve();
-                        });
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        return Promise.resolve();
-                    });
-        });
+            });
     }
 
     downloadSubtitle(url) {
